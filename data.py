a038c3f1bb977231f0f6d5fd376f98ba8879f8e7
Sam Pomerantz <sam@sampomerantz.me>
2022-11-19 00:53:56 -0500

    added fcf and more methods

--- data.py
+++ data.py
@@ -20,7 +20,34 @@ def getMarketPrice(stock: str):
     return ticker.info['regularMarketPrice']
 
 
-def getMarketAnnualReturn():
-    returns = [-20.09, 28.71, 18.4, 31.49, -4.38, 21.83, 11.96,
-               1.38, 13.69, 32.39, 16, 2.11, 15.06, 26.46, -37, 5.49]
-    return (sum(returns)/len(returns))
+def getFreeCashFlow(stock: str):
+    ticker = yf.Ticker(stock)
+    cashflow = ticker.cashflow
+    fcf = cashflow.loc['Total Cash From Operating Activities'] + \
+        cashflow.loc['Capital Expenditures']
+    return [float("{:.2f}".format(x)) for x in fcf.to_numpy()]
+
+
+def getBeta(stock: str):
+    ticker = yf.Ticker(stock)
+    return ticker.info['beta']
+
+
+def getRiskFreeRate():
+    response = requests.get(
+        url='https://ycharts.com/indicators/1_year_treasury_rate', headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh;'})
+
+    index1 = response.text.find('<p>1 Year Treasury Rate is at ')+30
+    index2 = response.text.find('%', index1)
+    return float(response.text[index1:index2])
+
+
+def getMarketMonthlyReturn():
+    response = requests.get(
+        url='https://ycharts.com/indicators/sp_500_monthly_return', headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh;'})
+
+    index1 = response.text.find('<p>S&amp;P 500 Monthly Return is at ')+36
+    index2 = response.text.find('%', index1)
+    return float(response.text[index1:index2])
--- methods.py
+++ methods.py
@@ -1,5 +1,6 @@
 import math
 from data import *
+import sys
 # https://www.omnicalculator.com/finance/intrinsic-value
 # https://www.fool.com/investing/how-to-invest/stocks/discounted-cash-flow-model/
 # http://www.moneychimp.com/articles/valuation/dcf.htm
@@ -11,7 +12,11 @@ def dcf(stock: str):
     y1 = 5  # grow at growth rate for y1 years
     g1 = 7.79/100  # growth rate
     g2 = 5/100  # level off to annual growth rate
-    Kc = getMarketAnnualReturn()/100  # market benchmark
+
+    returns = [-20.09, 28.71, 18.4, 31.49, -4.38, 21.83, 11.96,
+               1.38, 13.69, 32.39, 16, 2.11, 15.06, 26.46, -37, 5.49]
+    tmp = (sum(returns)/len(returns))
+    Kc = tmp()/100  # market benchmark
 
     v1 = eps*geomSeries((1 + g1)/(1 + Kc), 1, y1)
     v2 = futureValue(eps, g1, y1)*(1 + g2)/(Kc - g2)
@@ -38,5 +43,52 @@ def presentValue(fv, r, y):
     return fv/math.pow(1+r, y)
 
 
-a = dcf('AAPL')
-print(a)
+def cashFlowValuation(stock: str):
+    ticket = yf.Ticker(stock)
+    outstandingshares = ticket.info['sharesOutstanding']
+
+    beta = getBeta(stock)
+    adjusted_beta = (1-0.3333) * beta + 0.3333
+    risk_free_rate = getRiskFreeRate()/100  # 1 year treasury bill rate
+    market_return = getMarketMonthlyReturn()/100  # 1 year S&P 500 return / 12
+    expected_return = (risk_free_rate +
+                       adjusted_beta * (market_return - risk_free_rate))
+
+    required_rate = expected_return
+    perpetual_rate = 0.02
+    cashflow_growthrate = 0.03
+
+    years = [1, 2, 3, 4]
+
+    freecashflow = getFreeCashFlow(stock)
+    freecashflow.reverse()
+
+    futurefreecashflow = []
+    discountfactor = []
+    discountedfuturecashflow = []
+
+    terminalvalue = freecashflow[-1] * \
+        (1 + perpetual_rate) / (required_rate - perpetual_rate)
+
+    for year in years:
+        cashflow = freecashflow[-1] * (1 + cashflow_growthrate) ** year
+        futurefreecashflow.append(cashflow)
+        discountfactor.append((1 + required_rate) ** year)
+
+    for i in range(0, len(years)):
+        discountedfuturecashflow.append(
+            futurefreecashflow[i] / discountfactor[i])
+
+    discountedterminalvalue = terminalvalue/(1+required_rate)**len(years)
+    discountedfuturecashflow.append(discountedterminalvalue)
+
+    todayvalue = sum(discountedfuturecashflow)
+    fairvalue = todayvalue/outstandingshares
+
+    marketPrice = getMarketPrice(stock)
+    print(f'The Market Price of {stock} is ${marketPrice}')
+    print(f'The Fair Value is ${fairvalue:.2f}')
+    if (marketPrice > fairvalue):
+        print(f"It's Overvalued by ${marketPrice - fairvalue:.2f}")
+    else:
+        print(f"It's Undervalued by ${fairvalue - marketPrice:.2f}")
